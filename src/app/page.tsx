f46c86ca5f7f8a0b32874bfbf5b2f1cea1a5b8ae
ragulrathesh <ragulrathesh815@gmail.com>
2024-05-24 16:37:31 +0530

    htmlcodes added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>ragul rathesh</h1>
-      <p>hello everyone</p>
-      <p>bye</p>
+      <h1>User Input Web Page</h1>
+      <p>Enter a number to print:</p>
+      <input type="nuber" id="num1" placeholder="Number1"></input>
+      <button id="printbtn">Print</button>
+      <h3>You entered:<output id="enterednumber"></output></h3>
     </main>
   );
 }
